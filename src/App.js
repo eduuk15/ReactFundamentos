dd982c60c98e378544ec971668fe0fc7f424a5a4
Eduardo <eduardo@conectra.com.br>
2023-01-16 16:36:14 -0300

    entendendo o funcionamento das props no react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 
-const category = 'Posts da semana';
+import Post from './Post';
 
 function App() {
     return (
         <>
             <h1>JStack's Blog</h1>
-            <h2>{category}</h2>
+            <h2>Posts da semana</h2>
+
+            <hr />
+
+            <Post
+                title="Título da notícia 1"
+                subtitle="Subtítulo da notícia 1"
+            />
+            <Post
+                title="Título da notícia 2"
+                subtitle="Subtítulo da notícia 2"
+            />
+            <Post
+                title="Título da notícia 3"
+                subtitle="Subtítulo da notícia 3"
+            />
+            <Post
+                title="Título da notícia 4"
+                subtitle="Subtítulo da notícia 4"
+            />
         </>
     );
 }
