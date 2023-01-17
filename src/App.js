2d227040367449c60778bf98d2b01ad4d85c3942
Eduardo <eduardo@conectra.com.br>
2023-01-17 15:21:10 -0300

    entendendo o funcionamento do use state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 import Post from './Post';
 
-const posts = [
-    { title: 'Title1', subtitle: 'Sub1', likes: 20 },
-    { title: 'Title2', subtitle: 'Sub2', likes: 40 },
-    { title: 'Title3', subtitle: 'Sub3', likes: 50 },
-]
-
 function App() {
+    const [posts, setPosts] = useState([
+        { id: Math.random(), title: 'Title1', subtitle: 'Sub1', likes: 20 },
+        { id: Math.random(), title: 'Title2', subtitle: 'Sub2', likes: 40 },
+        { id: Math.random(), title: 'Title3', subtitle: 'Sub3', likes: 50 },
+    ]);
+
+    function handleRefresh() {
+        setPosts((prevState) => [
+            ...prevState,
+            {
+                id: Math.random(),
+                title: `Title${prevState.length + 1}`,
+                subtitle: `Sub${prevState.length + 1}`,
+                likes: 50
+            }
+        ])
+    }
+
     return (
         <>
             <Header>
-                <h2>Posts da semana</h2>
+                <h2>
+                    Posts da semana
+                    <button onClick={handleRefresh}>Atualizar</button>
+                </h2>
             </Header>
 
             <hr />
 
             {posts.map(post => (
                 <Post
-                    key={post.title}
+                    key={post.id}
                     post={{
                         title: post.title,
                         subtitle: post.subtitle
