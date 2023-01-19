52c3b9bfbd51fba7c7e02a1b33528e1cbf956cab
Eduardo <eduardo@conectra.com.br>
2023-01-19 16:56:50 -0300

    entendendo a estilizacao por inline styles

--- src/Header.js
+++ src/Header.js
@@ -8,11 +8,17 @@ export default function Header({ title, children }) {
     const { onToggleTheme } = useContext(ThemeContext);
 
     return (
-        <>
+        <div
+            style={{
+                background: "#ccc",
+                fontFamily: 'sans-serif',
+                marginBottom: '24px'
+            }}
+        >
             <h1>{title}</h1>
             <Button onClick={onToggleTheme}>Mudar tema</Button>
             {children}
-        </>
+        </div>
     )
 }
 
