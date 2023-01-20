8c4697c4bc762f7f19735f2f1229549b3551d1a7
Eduardo <eduardo@conectra.com.br>
2023-01-20 16:42:10 -0300

    organizando a estrutura do projeto e entendendo os estilos globais com styled-components

---
+++ src/components/App/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Header from "../Header";
+
+import Post from '../Post';
+
+import { ThemeProvider } from "../../context/ThemeContext";
+
+import styles from './App.scss';
+
+function App() {
+
+    const [posts, setPosts] = useState([
+        { id: Math.random(), title: 'Title1', subtitle: 'Sub1', likes: 20, read: false, removed: true },
+        { id: Math.random(), title: 'Title2', subtitle: 'Sub2', likes: 40, read: true, removed: false },
+        { id: Math.random(), title: 'Title3', subtitle: 'Sub3', likes: 50, read: false, removed: false  },
+    ]);
+
+    function handleRefresh() {
+        setPosts((prevState) => [
+            ...prevState,
+            {
+                id: Math.random(),
+                title: `Title${prevState.length + 1}`,
+                subtitle: `Sub${prevState.length + 1}`,
+                read: false,
+                likes: 50
+            }
+        ])
+    }
+
+    function handleRemovePost(postId) {
+        setPosts((prevState) => prevState.map(
+            post => (
+                post.id === postId ? { ...post, removed: true } : post
+            )
+        ))
+    }
+
+    return (
+        <ThemeProvider>
+            <Header>
+                <h2 className={styles.title}>
+                    Posts da semana
+                    <button onClick={handleRefresh}>Atualizar</button>
+                </h2>
+            </Header>
+
+            <hr />
+
+            {posts.map(post => (
+                <Post
+                    key={post.id}
+                    post={post}
+                    onRemove={handleRemovePost}
+                />
+            ))}
+        </ThemeProvider>
+    );
+}
+
+export default App;
