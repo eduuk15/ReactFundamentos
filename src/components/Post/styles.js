3c71eedc87fd74530f84886ae9286cf7541c2bda
Eduardo <eduardo@conectra.com.br>
2023-02-01 13:27:19 -0300

    entendendo a definicao de estilos dentro de uma unica funcao

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,9 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.article`
     margin-bottom: 24px;
 
-    opacity: ${(props) => props.removed ? 0.5 : 1};
+    ${(props) => css`
+        color: ${props.removed ? '#f00' : '#fff'};
+        opacity: ${props.removed ? 0.5 : 1};
+    `}
 `;
 
 export const Subtitle = styled.small`
